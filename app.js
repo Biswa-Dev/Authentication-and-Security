e1bcbc61213645ae6cb99ab4047d11462878629f
Biswajit Chanda <74866208+Biswa-Dev@users.noreply.github.com>
2021-09-01 20:30:09 +0530

    Used Environment variable to keep secrets safe

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //requiring all the required packages
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -23,6 +24,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ['password']});
 
 //creating model
 const User = new mongoose.model("User",userSchema);
