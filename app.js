832c6c1899fdb3b5237765b5115904ecaff89c1c
Biswajit Chanda <74866208+Biswa-Dev@users.noreply.github.com>
2021-09-06 00:17:59 +0530

    level-4 Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
 const port = process.env.PORT || 3000;
+const saltRounds = 10;
 
 //creating our app
 const app = express();
@@ -43,32 +44,36 @@ app.get("/login",function(req,res){
 
 //post method for register route
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render('secrets');
-        }
-    });
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render('secrets');
+            }
+        });
+    }) 
 });
 //post method for login route
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email: username},function(err,foundUser){
         if(err){
             console.log(err);
         }else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render('secrets');
-                }else{
-                    console.log("Incorrect Password!");
-                }
+                bcrypt.compare(password,foundUser.password,function(err,result) {
+                    if(result === true){
+                        res.render('secrets');
+                    }else{
+                        console.log("Incorrect Password!");
+                    }    
+                });
             }else{
                 console.log("Invalid email");
             }
