e87f2200695ff05a32523664d5b834cf1ebf35d4
Biswajit Chanda <74866208+Biswa-Dev@users.noreply.github.com>
2021-09-06 20:21:43 +0530

    Level 6 Adding Cookies and Session using Passport.js

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const e = require('express');
 const port = process.env.PORT || 3000;
-const saltRounds = 10;
+
 
 //creating our app
 const app = express();
@@ -14,6 +17,18 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+//use session package
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized:false
+}));
+
+//initializing passport
+app.use(passport.initialize());
+//using session with passport
+app.use(passport.session());
 
 //connetion with database userDB
 mongoose.connect("mongodb://localhost:27017/userDB",{
@@ -26,8 +41,17 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+//adding passport-local-mongoose as plugin to userSchema
+userSchema.plugin(passportLocalMongoose);
+
 //creating model
 const User = new mongoose.model("User",userSchema);
+
+//using the plugin to create strategy to authenticate user
+passport.use(User.createStrategy());
+//to create and crumble cookies using serialize and deserialize
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //get method for home route
 app.get("/",function(req,res){
@@ -41,42 +65,46 @@ app.get("/register",function(req,res){
 app.get("/login",function(req,res){
     res.render('login');
 });
+//get method for secrets route
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    }else{
+        res.redirect('/login');
+    }    
+});
+//get method for logout
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 
 //post method for register route
 app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render('secrets');
-            }
-        });
-    }) 
+    User.register({username: req.body.username},req.body.password,function(err,user){
+       if(err){
+           console.log(err);
+           res.redirect("/register");
+       }else{
+           passport.authenticate("local")(req,res,function(){
+               res.redirect("/secrets");
+           });
+        } 
+    });
 });
 //post method for login route
 app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: username},function(err,foundUser){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user,function(err){
         if(err){
             console.log(err);
         }else{
-            if(foundUser){
-                bcrypt.compare(password,foundUser.password,function(err,result) {
-                    if(result === true){
-                        res.render('secrets');
-                    }else{
-                        console.log("Incorrect Password!");
-                    }    
-                });
-            }else{
-                console.log("Invalid email");
-            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
     });
 });
